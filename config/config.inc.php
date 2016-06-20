c529a5aedc38bd664e4e8cc01cf0099297c9efe2
Tobiasz Kapusta <tobiasz.kapusta@bm.pl>
2016-06-20 10:58:00 +0200

    new version

--- config/config.inc.php
+++ config/config.inc.php
@@ -1,5 +1,5 @@
 <?php
-define('BP_VERSION','1.2.3');
+define('BP_VERSION','1.2.4');
 define('TEST_ADDRESS_URL','https://pay-accept.bm.pl/payment');
 define('PROD_ADDRESS_URL','https://pay.bm.pl/payment');
 define('HASH_SEPARATOR','|');
