3cc90a23c1bd236df4c5e42f40a9a7af384717b2
Agnieszka Kurzajewska <agnieszka.kurzajewska@cloudservices.pl>
2021-06-02 08:50:15 +0200

    new files added

---
+++ classes/dictionaries/OrderStatusMessageDictionary.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Dictionaries;
+
+abstract class OrderStatusMessageDictionary
+{
+    const PENDING = 1;
+
+    const ORDER_STATUS_MESSAGE = [
+        self::PENDING => 'Payment in progress',
+    ];
+
+    public static function getMessage($order_status_id)
+    {
+
+        return self::hasKey($order_status_id) ? self::ORDER_STATUS_MESSAGE[$order_status_id] : null;
+    }
+
+    private static function hasKey($order_status_id): bool
+    {
+        return array_key_exists($order_status_id, self::ORDER_STATUS_MESSAGE);
+    }
+}
