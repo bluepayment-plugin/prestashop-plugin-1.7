9784f76c2fc5d8af1e4a5727f1b8034797a70254
Coalacode.com <info@coalacode.com>
2022-05-02 02:06:16 +0200

    update vendors

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -6,7 +6,7 @@
     'aliases' => 
     array (
     ),
-    'reference' => '3d3135b5b39ca9317657c17c07e6b0dd53b9959f',
+    'reference' => '562312b762c659db4e1ac3b4f75ee7a254f9085b',
     'name' => 'prestashop/bluepayment',
   ),
   'versions' => 
@@ -19,24 +19,6 @@
       array (
       ),
       'reference' => 'aeb7d8ea49c7963d3b581378955dbf5bc49aa320',
-    ),
-    'knplabs/github-api' => 
-    array (
-      'pretty_version' => 'v2.20.0',
-      'version' => '2.20.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '939869394c6414768547685945fdba4fe3f061b5',
-    ),
-    'nikic/php-parser' => 
-    array (
-      'pretty_version' => 'v3.1.0',
-      'version' => '3.1.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '4d4896e553f2094e657fe493506dc37c509d4e2b',
     ),
     'nyholm/psr7' => 
     array (
@@ -53,24 +35,6 @@
       array (
         0 => '1.0',
       ),
-    ),
-    'php-http/cache-plugin' => 
-    array (
-      'pretty_version' => '1.7.5',
-      'version' => '1.7.5.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '63bc3f7242825c9a817db8f78e4c9703b0c471e2',
-    ),
-    'php-http/client-common' => 
-    array (
-      'pretty_version' => '2.5.0',
-      'version' => '2.5.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'd135751167d57e27c74de674d6a30cef2dc8e054',
     ),
     'php-http/client-implementation' => 
     array (
@@ -140,15 +104,6 @@
       ),
       'reference' => '4c4c1f9b7289a2ec57cde7f1e9762a5789506f88',
     ),
-    'prestashop/autoindex' => 
-    array (
-      'pretty_version' => 'v1.0.0',
-      'version' => '1.0.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '92e10242f94a99163dece280f6bd7b7c2b79c158',
-    ),
     'prestashop/bluepayment' => 
     array (
       'pretty_version' => 'dev-master',
@@ -156,16 +111,7 @@
       'aliases' => 
       array (
       ),
-      'reference' => '3d3135b5b39ca9317657c17c07e6b0dd53b9959f',
-    ),
-    'psr/cache' => 
-    array (
-      'pretty_version' => '1.0.1',
-      'version' => '1.0.1.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'd11b50ad223250cf17b86e38383413f5a6764bf8',
+      'reference' => '562312b762c659db4e1ac3b4f75ee7a254f9085b',
     ),
     'psr/http-client' => 
     array (
@@ -215,51 +161,6 @@
         0 => '1.0',
       ),
     ),
-    'psr/log' => 
-    array (
-      'pretty_version' => '1.1.4',
-      'version' => '1.1.4.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'd49695b909c3b7628b6289db5479a1c204601f11',
-    ),
-    'squizlabs/php_codesniffer' => 
-    array (
-      'pretty_version' => '3.6.2',
-      'version' => '3.6.2.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '5e4e71592f69da17871dba6e80dd51bce74a351a',
-    ),
-    'symfony/console' => 
-    array (
-      'pretty_version' => 'v3.4.0',
-      'version' => '3.4.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '9468ad3fba3a5e1f0dc12a96e50e84cddb923cf0',
-    ),
-    'symfony/debug' => 
-    array (
-      'pretty_version' => 'v4.4.37',
-      'version' => '4.4.37.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '5de6c6e7f52b364840e53851c126be4d71e60470',
-    ),
-    'symfony/finder' => 
-    array (
-      'pretty_version' => 'v3.4.0',
-      'version' => '3.4.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'dac8d7db537bac7ad8143eb11360a8c2231f251a',
-    ),
     'symfony/options-resolver' => 
     array (
       'pretty_version' => 'v4.4.37',
@@ -268,15 +169,6 @@
       array (
       ),
       'reference' => '41d1e741a292574887629369400820c9645e8a87',
-    ),
-    'symfony/polyfill-mbstring' => 
-    array (
-      'pretty_version' => 'v1.24.0',
-      'version' => '1.24.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '0abb51d2f102e00a4eefcf46ba7fec406d245825',
     ),
     'symfony/polyfill-php80' => 
     array (
